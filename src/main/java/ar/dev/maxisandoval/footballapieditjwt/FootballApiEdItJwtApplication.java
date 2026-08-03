2458aa591bd3683129deccaa83aafc4c5a99c6a1
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-08-03 20:57:10 -0300

    first commit

---
+++ src/main/java/ar/dev/maxisandoval/footballapieditjwt/FootballApiEdItJwtApplication.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.footballapieditjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballApiEdItJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballApiEdItJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/dev/maxisandoval/footballapieditjwt/FootballApiEdItJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.footballapieditjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApiEdItJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

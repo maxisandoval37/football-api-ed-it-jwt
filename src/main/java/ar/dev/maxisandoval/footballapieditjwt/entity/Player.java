f09b016a990c79323b05aae04c73329526115e3c
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-08-05 20:59:54 -0300

    feat (jpa) se agregan las entidades

---
+++ src/main/java/ar/dev/maxisandoval/footballapieditjwt/entity/Player.java
@@ -0,0 +1,50 @@
+package ar.dev.maxisandoval.footballapieditjwt.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Player(Long id, String name, Team team) {
+        this.id = id;
+        this.name = name;
+        this.team = team;
+    }
+
+    public Player() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
---
+++ src/main/java/ar/dev/maxisandoval/footballapieditjwt/entity/Team.java
@@ -0,0 +1,19 @@
+package ar.dev.maxisandoval.footballapieditjwt.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer capacity;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Player> players;
+}

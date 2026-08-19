f0c816cba38f61993a5e595578d7340604594b0e
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-08-19 20:35:01 -0300

    feat(h2) se habilita la consola de h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO team (name, capacity) VALUES ('trueno verde', 100000);
+INSERT INTO team (name, capacity) VALUES ('boca juniors', 54000);
+INSERT INTO team (name, capacity) VALUES ('river plate', 84567);
+INSERT INTO team (name, capacity) VALUES ('racing club', 42000);
+INSERT INTO team (name, capacity) VALUES ('independiente', 48069);
+INSERT INTO team (name, capacity) VALUES ('san lorenzo', 47964);
+INSERT INTO team (name, capacity) VALUES ('rosario central', 46955);
+INSERT INTO team (name, capacity) VALUES ('newells old boys', 42000);
+INSERT INTO team (name, capacity) VALUES ('estudiantes', 32530);
+INSERT INTO team (name, capacity) VALUES ('gimnasia lp', 30970);
+
+INSERT INTO player (name, team_id) VALUES ('Priscila', 1);
+INSERT INTO player (name, team_id) VALUES ('Lucas', 1);
+INSERT INTO player (name, team_id) VALUES ('Nico', 1);
+INSERT INTO player (name, team_id) VALUES ('Maxi', 1);

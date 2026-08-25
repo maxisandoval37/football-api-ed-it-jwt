849712ab5b7bc869cca8a55ca3fbbbbd1dd24dd3
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-08-24 21:20:06 -0300

    feat(validaciones) validaciones por medio de anotaciones

--- src/main/java/ar/dev/maxisandoval/footballapieditjwt/entity/Player.java
+++ src/main/java/ar/dev/maxisandoval/footballapieditjwt/entity/Player.java
@@ -1,6 +1,8 @@
 package ar.dev.maxisandoval.footballapieditjwt.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -14,6 +16,8 @@ public class Player {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
     private String name;
 
     @ManyToOne
--- src/main/java/ar/dev/maxisandoval/footballapieditjwt/entity/Team.java
+++ src/main/java/ar/dev/maxisandoval/footballapieditjwt/entity/Team.java
@@ -1,6 +1,7 @@
 package ar.dev.maxisandoval.footballapieditjwt.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import java.util.List;
 
@@ -15,8 +16,12 @@ public class Team {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
     private String name;
 
+    @NotNull
+    @Min(value = 0, message = "La capacidad no puede ser negativa")
     private Integer capacity;
 
     @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)

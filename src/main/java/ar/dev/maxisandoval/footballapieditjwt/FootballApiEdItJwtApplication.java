81c0377231eaaa02f001ed82fb8095fa691135e8
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-08-26 20:19:15 -0300

    feat(cache) se implementa cache en el service

--- src/main/java/ar/dev/maxisandoval/footballapieditjwt/FootballApiEdItJwtApplication.java
+++ src/main/java/ar/dev/maxisandoval/footballapieditjwt/FootballApiEdItJwtApplication.java
@@ -2,8 +2,10 @@ package ar.dev.maxisandoval.footballapieditjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FootballApiEdItJwtApplication {
 
     public static void main(String[] args) {
--- src/main/java/ar/dev/maxisandoval/footballapieditjwt/service/TeamService.java
+++ src/main/java/ar/dev/maxisandoval/footballapieditjwt/service/TeamService.java
@@ -4,6 +4,7 @@ import ar.dev.maxisandoval.footballapieditjwt.entity.Team;
 import ar.dev.maxisandoval.footballapieditjwt.repository.TeamRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -13,18 +14,22 @@ public class TeamService {
 
     private final TeamRepository teamRepository;
 
+    @Cacheable("teams")
     public List<Team> getAll() {
         return teamRepository.findAll();
     }
 
+    @Cacheable(value = "team", key = "#id")
     public Team getById(Long id) {
         return teamRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Team not found with id: "+id));
     }
 
+    @CachePut(value = "team", key = "#result.id")
     public Team save(Team team) {
         return teamRepository.save(team);
     }
 
+    @CachePut(value = "team", key = "#id")
     public Team update(Long id, Team updatedTeam) {
         Team existingTeam = getById(id);
 
@@ -34,10 +39,12 @@ public class TeamService {
         return teamRepository.save(existingTeam);
     }
 
+    @CacheEvict(value = "teams", allEntries = true)
     public void deleteById(Long id) {
         teamRepository.deleteById(id);
     }
 
+    @CacheEvict(value = "teams", allEntries = true)
     public void deleteAll() {
         teamRepository.deleteAll();
     }

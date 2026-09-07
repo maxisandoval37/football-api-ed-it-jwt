e2b0668bbc06137f321679d8aacc26d2aee0708c
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-09-07 20:17:37 -0300

    feat(security) agregado controlador

--- src/main/java/ar/dev/maxisandoval/footballapieditjwt/controller/AuthController.java
+++ src/main/java/ar/dev/maxisandoval/footballapieditjwt/controller/AuthController.java
@@ -1,14 +1,27 @@
 package ar.dev.maxisandoval.footballapieditjwt.controller;
 
+import ar.dev.maxisandoval.footballapieditjwt.dto.*;
+import ar.dev.maxisandoval.footballapieditjwt.service.AuthService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
-    //private final AuthService
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody AuthRequest request) {
+        String token = authService.register(request.getUsername(), request.getPassword());
+        return new AuthResponse(token);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        String token = authService.authenticate(request.getUsername(), request.getPassword());
+        return new AuthResponse(token);
+    }
 
 }
--- src/main/java/ar/dev/maxisandoval/footballapieditjwt/dto/AuthResponse.java
+++ src/main/java/ar/dev/maxisandoval/footballapieditjwt/dto/AuthResponse.java
@@ -1,5 +1,10 @@
 package ar.dev.maxisandoval.footballapieditjwt.dto;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
     private String token;
 }
